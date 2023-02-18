544a030ab918445ce829a98b5ee0a47e5fd6120b
Martin Eriksen <m@rtin.bz>
2023-02-18 17:59:46 -0300

    message

--- Exercise_25.rkt
+++ Exercise_25.rkt
@@ -1,7 +1,14 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise_24) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
-(define (==> x y)
-  (or (not x) y))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise_25) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define cat (rectangle 10 5 "solid" "chocolate"))
 
-(==> #true #false)
+
+(define (image-classify img)
+  (cond
+    [(>= (image-height img) (image-width img)) "tall"]
+    [(= (image-height img) (image-width img)) "square"]
+    [(<= (image-height img) (image-width img)) "wide"]))
+
+
+(image-classify cat)

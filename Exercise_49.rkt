24eca0522e802a08bafb0120bc515e8495ba9846
gggravity <m@rtin.bz>
2023-02-23 20:29:19 -0300

    message

--- Exercise_49.rkt
+++ Exercise_49.rkt
@@ -1,8 +1,33 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise_49) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
 (define (my-exp y)
   (- 200 (cond [(> y 200) 0] [else y])))
 
 (my-exp 100)
 (my-exp 210)
+
+;; rocket part
+
+
+(define WIDTH  100)
+(define HEIGHT  60)
+(define MTSCN  (empty-scene WIDTH HEIGHT))
+(define ROCKET (bitmap "images/rocket.png"))
+(define ROCKET-CENTER-TO-TOP
+  (- HEIGHT (/ (image-height ROCKET) 2)))
+
+(define (create-rocket-scene.v5 h)
+  (cond
+   [(<= h ROCKET-CENTER-TO-TOP)
+    (place-image ROCKET 50 h MTSCN)]
+   [(> h ROCKET-CENTER-TO-TOP)
+    (place-image ROCKET 50 ROCKET-CENTER-TO-TOP MTSCN)]))
+
+(define (within h)
+    (cond
+     [(<= h ROCKET-CENTER-TO-TOP) h]
+     [else ROCKET-CENTER-TO-TOP]))
+
+(define (create-rocket-scene.v6 h)
+    (place-image ROCKET 50 (within h) MTSCN))
+
+;; (create-rocket-scene.v5 (/ HEIGHT 2))
+(create-rocket-scene.v6 100)

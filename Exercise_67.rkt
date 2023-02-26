a3dc1cf7057257cee98298d13927ef5bdcae5d10
gggravity <m@rtin.bz>
2023-02-26 12:56:25 -0300

    message

---
+++ Exercise_67.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex063) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(require 2htdp/image)
+(require 2htdp/universe)
+
+(define SPEED 3)
+(define-struct balld [location direction])
+(make-balld 10 "up")
+(make-balld 20 "down")

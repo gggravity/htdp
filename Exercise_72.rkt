673682440cc510f224475dcc3d7e5a5b3acad502
gggravity <m@rtin.bz>
2023-02-26 14:17:21 -0300

    message

---
+++ Exercise_72.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex063) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(require 2htdp/image)
+(require 2htdp/universe)
+
+(define-struct phone [area number])
+
+(make-phone 123 "123-123-123")
+
+(define-struct phone# [area switch num])
+
+(make-phone# 123 123 "123-123-123") 

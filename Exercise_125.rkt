a2b43b2b264476c5ffffc9b41b5122d52a1ab168
gggravity <m@rtin.bz>
2023-03-19 18:25:23 -0300

    message

---
+++ Exercise_125.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex063) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(define-struct oops []) ; legal
+
+(define-struct child [parents dob date]) ; legal
+
+;; (define-struct (child person) [dob date]) ; iligal, (child person) should be a structure name
+
+(make-oops ) ; empty structure

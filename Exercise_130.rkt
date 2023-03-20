7883686dfae51c4c7e67bbfbf5fcdfdc0e90e317
gggravity <m@rtin.bz>
2023-03-20 17:50:21 -0300

    message

---
+++ Exercise_130.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex063) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(cons "Findler"
+      (cons "Flatt"
+            (cons "Felleisen"
+                  (cons "Krishnamurthi"
+                        '()))))
+
+(cons "1" (cons "2" '()))
+(cons 2 '())
+
+;; (cons "1" (cons "2" '())) is a list of names, because "1" it a string or another way of writing one.
+;; (cons 2 '()) is not a list of names, because 2 is the integer 2 and not the name two.

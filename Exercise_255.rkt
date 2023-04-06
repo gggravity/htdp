8b146109915d4802f70507c94889eea4c4f1f0b0
gggravity <m@rtin.bz>
2023-04-06 11:19:26 -0200

    message

---
+++ Exercise_255.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname rkt) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+
+;; [List-of Number] [Number -> Number] -> [List-of Number]
+(define (map-n lon fn)
+  lon)
+
+;; [List-of String] [String -> String] -> [List-of String]
+(define (map-s los fn)
+  los)
+

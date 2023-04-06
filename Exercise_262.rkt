73072eae25f6869adbe85019ff59db030f779e3b
gggravity <m@rtin.bz>
2023-04-06 19:20:46 -0200

    message

--- Exercise_262.rkt
+++ Exercise_262.rkt
@@ -2,35 +2,26 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname rkt) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
 
-;; '((1 0 0)
-;;   (0 1 0)
-;;   (0 0 1))
-
-(define (make-row i n)
-  (if (=  n 0) '()
-      (if (= n i)
-          (cons 1 (make-row i (sub1 n)))
-          (cons 0 (make-row i (sub1 n)))
-          )))
-
-(check-expect (make-row 3 3) '(1 0 0))
-
-(define (make-im-table i n)
-  (if (= i 0) '()
-      (cons (make-row i n) (make-im-table (sub1 i) n))
-      ))
-
-(check-expect (make-im-table 3 3) '((1 0 0)
-                                    (0 1 0)
-                                    (0 0 1)))
-
 (define (identity-matrix n)
-  (make-im-table n n))
+  (local (
+          (define (make-row i n)
+            (if (=  n 0) '()
+                (if (= n i)
+                    (cons 1 (make-row i (sub1 n)))
+                    (cons 0 (make-row i (sub1 n)))
+                    )))
+          (define (make-im-table i n)
+            (if (= i 0) '()
+                (cons (make-row i n) (make-im-table (sub1 i) n))
+                ))
+          )
+    (make-im-table n n)
+    )
+  )
 
 (check-expect (identity-matrix 0) '())
 
 (check-expect (identity-matrix 1) '((1)))
-
 
 (check-expect (identity-matrix 3) '((1 0 0)
                                     (0 1 0)

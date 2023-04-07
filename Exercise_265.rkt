0559f88adcfd8b01b84eedf3e23b75649276eed7
gggravity <m@rtin.bz>
2023-04-07 11:10:26 -0200

    message

---
+++ Exercise_265.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname rkt) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+
+((local ((define (f x) (+ (* 4 (sqr x)) 3))) f) 1)
+
+((λ (x) (+ (* 4 (sqr x)) 3)) 1)
+
+(+ (* 4 (sqr 1)) 3)
+
+(+ (* 4 1) 3)
+
+(+ 4 3)

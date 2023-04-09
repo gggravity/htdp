7ed6ba0bc5494e26a59abe59bfecefc433c10d93
gggravity <m@rtin.bz>
2023-04-09 21:10:09 -0200

    message

--- Exercise_292.rkt
+++ Exercise_292.rkt
@@ -2,13 +2,91 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname rkt) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
 
-
 (define (sorted cmp)
   (λ (l)
     #true))
 
-; [X X -> Boolean] [NEList-of X] -> Boolean 
-; determines whether l is sorted according to cmp
+(define (sortet>? l)
+  (cond
+    [(empty? (rest l)) #true]
+    [else
+     (and
+      (> (first l) (first (rest l)))
+      (sortet>? (rest l))
+      )]))
+
+;; List-of-numbers -> List-of-numbers
+;; produces a sorted version of alon
+(define (sort> alon)
+  (cond
+    [(empty? alon) '()]
+    [else (insert (first alon) (sort> (rest alon)))]
+    ))
+
+(check-expect (sort> '()) '())
+(check-expect (sort> (list 3 2 1)) (list 3 2 1))
+(check-expect (sort> (list 1 2 3)) (list 3 2 1))
+(check-expect (sort> (list 12 20 -5)) (list 20 12 -5))
+
+(check-satisfied (sort> (list 3 2 1)) sortet>?)
+(check-satisfied (sort> (list 1 2 3)) sortet>?)
+(check-satisfied (sort> (list 12 20 -5)) sortet>?)
+
+;; Number List-of-number -> List-of-numbers
+;; inserts n into the sorted list of numbers alon
+(define (insert n alon)
+  (cond
+    [(empty? alon) (cons n '())]
+    [else (if (>= n (first alon))
+              (cons n alon)
+              (cons (first alon) (insert n (rest alon)))
+              )]))
+
+(check-expect (insert 5 (list 6)) (list 6 5))
+(check-expect (insert 5 (list 4)) (list 5 4))
+(check-expect (insert 12 (list 20 -5)) (list 20 12 -5))
+
+
+; [X] [List-of X] [X X -> Boolean] -> [List-of X]
+; sorts alon0 according to cmp
+
+(check-expect (sort-cmp '("c" "b") string<?) '("b" "c"))
+(check-expect (sort-cmp '(2 1 3 4 6 5) <) '(1 2 3 4 5 6))
+
+(define (sort-cmp alon0 cmp)
+  (local (; [List-of X] -> [List-of X]
+          ; produces a variant of alon sorted by cmp
+          (define (isort l)
+            (cond
+              [(empty? l) '()]
+              [else (insert (first l) (isort (rest l)))]
+              ))
+
+          ; X [List-of X] -> [List-of X]
+          ; inserts n into the sorted list of numbers alon 
+          (define (insert n l)
+            (cond
+              [(empty? l) (cons n '())]
+              [else (if (cmp n (first l))
+                        (cons n l)
+                        (cons (first l) (insert n (rest l)))
+                        )])))
+    (isort alon0)))
+
+;; letter-counts List-of-letter-counts -> List-of-letter-counts
+;; inserts n into the sorted list of letter-counts l
+
+(check-satisfied (sort-cmp '("c" "b") string<?)
+                 (sorted string<?))
+(check-satisfied (sort-cmp '(2 1 3 4 6 5) <)
+                 (sorted <))
+
+(check-expect [(sorted string<?) '("b" "c")] #true)
+(check-expect [(sorted <) '(1 2 3 4 5 6)] #true)
+
+
+;; [X X -> Boolean] [NEList-of X] -> Boolean 
+;; determines whether l is sorted according to cmp
 
 (define (sorted? cmp l)
   (if (empty? (rest l)) #true
---
+++ Exercise_293.rkt
@@ -0,0 +1,51 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname rkt) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+
+
+(define l1 '(2 3 1 9 5 8 4 0 1 4))
+
+; X [List-of X] -> [Maybe [List-of X]]
+; returns the first sublist of l that starts
+; with x, #false otherwise
+(define (find x l)
+  (cond
+    [(empty? l) #false]
+    [else
+     (if (equal? (first l) x) l (find x (rest l)))]))
+
+(check-expect (find 9 l1) '(9 5 8 4 0 1 4))
+(check-expect (find 1 l1) '(1 9 5 8 4 0 1 4))
+(check-expect (find 0 l1) '(0 1 4))
+(check-expect (find 06 l1) #false)
+
+(define (found-another-way? x l)
+  (local ((define (find-index i x l)
+            (if (empty? l) -1
+                (if (equal? x (first l)) i
+                    (find-index (add1 i) x (rest l)))))
+          (define (sublist i l)
+            (cond
+              [(zero? i) l]
+              [(negative? i) #false]
+              [else (sublist (sub1 i) (rest l))])))
+    (sublist (find-index 0 x l) l)))
+
+
+
+(define (found? cmp)
+  (equal? (found-another-way? 9 l1) cmp))
+
+;; (sublist (find-index 0 9 l1) l1)
+(check-expect (found-another-way? 9 l1) '(9 5 8 4 0 1 4))
+(check-expect (found-another-way? 1 l1) '(1 9 5 8 4 0 1 4))
+(check-expect (found-another-way? 0 l1) '(0 1 4))
+
+(check-expect (find 9 l1) (found-another-way? 9 l1))
+(check-expect (find 3 l1) (found-another-way? 3 l1))
+(check-expect (find 6 l1) (found-another-way? 6 l1))
+
+(check-satisfied (find 9 l1) found?)
+;; (check-satisfied (find 3 l1) found?)
+;; (check-satisfied (find 6 l1) found?)
+

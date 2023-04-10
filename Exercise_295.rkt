ff36490911ddd34dd59e09b31719ffd9ef5e11fc
gggravity <m@rtin.bz>
2023-04-09 22:00:31 -0200

    message

---
+++ Exercise_295.rkt
@@ -0,0 +1,29 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname rkt) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+
+; distances in terms of pixels 
+(define WIDTH 300)
+(define HEIGHT 300)
+
+; N -> [List-of Posn]
+; generates n random Posns in [0,WIDTH) by [0,HEIGHT)
+(define (random-posns n)
+  (build-list
+   n
+   (lambda (i)
+     (make-posn (random WIDTH) (random HEIGHT)))))
+
+(define (within? p)
+  (not (or (or (< (posn-x p) 0)
+               (> (posn-x p) WIDTH))
+           (or (< (posn-y p) 0)
+               (> (posn-y p) HEIGHT))))
+  )
+
+(define (n-inside-playground? n)
+  (λ (result) 
+    (andmap (λ (p) (within? p)) result)))
+
+(check-satisfied (random-posns 3)
+                 (n-inside-playground? 3))

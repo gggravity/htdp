72afa21827eea06f6c727db8e155081d08f6b2c6
gggravity <m@rtin.bz>
2023-04-15 15:36:47 -0200

    message

---
+++ Exercise_331.rkt
@@ -0,0 +1,27 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname rkt) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+
+(require 2htdp/abstraction)
+
+; A Dir.v1 (short for directory) is one of: 
+; – '()
+; – (cons File.v1 Dir.v1)
+; – (cons Dir.v1 Dir.v1)
+
+; A File.v1 is a String.
+
+(define TS
+  '(("Text" ("part1" "part2" "part3"))
+    ("read!")
+    ("Libs"
+     ("Code" ("hang" "draw"))
+     ("Docs" ("read!")))))
+
+(define (how-many dir)
+  (for/sum ([file dir])
+    (if (string? file) 1
+        (how-many file))))
+
+(check-expect (how-many TS) 11)
+

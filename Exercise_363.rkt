62cea7a7ab0c770ad9a30a88d44ac20dc1b60105
gggravity <m@rtin.bz>
2023-04-23 13:58:48 -0200

    message

---
+++ Exercise_363.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname rkt) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+
+(require 2htdp/abstraction)
+
+;; An Attribute is a list of two items:
+;; (cons Symbol (cons String '()))
+
+;; An Xexpr is a list: 
+;; – (cons Symbol [List-of Xexpr])
+;; – (cons Symbol (cons [List-of Attribute] [List-of Xexpr]))

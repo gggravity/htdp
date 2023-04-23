11c6b4016acadf090cfacffdca74c7e3d32a2da2
gggravity <m@rtin.bz>
2023-04-23 15:26:19 -0200

    message

---
+++ Exercise_366.rkt
@@ -0,0 +1,74 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname rkt) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+
+(require 2htdp/abstraction)
+
+(define a0 '((initial "X")))
+
+(define e0 '(machine))
+(define e1 `(machine ,a0))
+(define e2 '(machine (action)))
+(define e3 '(machine () (action)))
+(define e4 `(machine ,a0 (action) (action)))
+
+;; (define (list-of-attributes? x)
+;;   (cond
+;;     [(empty? x) #true]
+;;     [else
+;;      (local ((define possible-attribute (first x)))
+;;        (cons? possible-attribute))]))
+
+(define (list-of-attributes? x)
+  (if (empty? x) #true (cons? (first x))))
+
+;; (define (xexpr-attr xe)
+;;   (local ((define optional-loa+content (rest xe)))
+;;     (cond
+;;       [(empty? optional-loa+content) '()]
+;;       [else
+;;        (local ((define loa-or-x
+;;                  (first optional-loa+content)))
+;;          (if (list-of-attributes? loa-or-x)
+;;              loa-or-x
+;;              '()))])))
+
+
+(define (xexpr-attr xexpr)
+(if (empty? (rest xexpr)) '()
+    (if (list-of-attributes? (second xexpr)) (second xexpr) '())
+    ))
+
+(check-expect (xexpr-attr e0) '())
+(check-expect (xexpr-attr e1) '((initial "X")))
+(check-expect (xexpr-attr e2) '())
+(check-expect (xexpr-attr e3) '())
+(check-expect (xexpr-attr e4) '((initial "X")))
+
+;; (define (xexpr-name xexpr)
+;;   (if (empty? (rest xexpr)) '()
+;;       (if (symbol? (first xexpr)) (first xexpr) "ERROR")
+;;       ))
+
+(define (xexpr-name xexpr)
+  (match xexpr
+    ['() '()]
+    [(cons (? symbol?) _) (first xexpr)]
+    [_ (error "xexpr-name")]
+    ))
+
+(check-expect (xexpr-name `(machine ,a0 (action) (action))) 'machine)
+
+(define (xexpr-content xexpr)
+  (match xexpr
+    ['() '()]
+    [(cons (? symbol?) (cons (? list-of-attributes?) body)) body]
+    [(cons (? symbol?) body) body]
+    [_ (error "xexpr-content")]
+    ))
+
+(check-expect (xexpr-content e0) '())
+(check-expect (xexpr-content e1) '())
+(check-expect (xexpr-content e2) '((action)))
+(check-expect (xexpr-content e3) '((action)))
+(check-expect (xexpr-content e4) '((action) (action)))
